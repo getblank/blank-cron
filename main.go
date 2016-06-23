7eee338badaf152c71406bf56295ce9d6484cb49
ivahaev <ivahaev@gmail.com>
2016-06-23 21:46:19 +0500

    debug mode and version support

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"sync"
 	"time"
@@ -9,6 +10,12 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/getblank/wango"
 	"github.com/spf13/cobra"
+)
+
+var (
+	buildTime string
+	gitHash   string
+	version   = "0.0.9"
 )
 
 var (
@@ -32,24 +39,38 @@ type task struct {
 }
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+	if os.Getenv("BLANK_DEBUG") != "" {
+		log.SetLevel(log.DebugLevel)
+	}
+	var verFlag *bool
 	rootCmd := &cobra.Command{
 		Use:   "router",
 		Short: "Router for Blank platform",
 		Long:  "The next generation of web applications. This is the router subsytem.",
 		Run: func(cmd *cobra.Command, args []string) {
+			if *verFlag {
+				printVersion()
+				return
+			}
 			log.Info("Router started")
 			go connectToTaskQ()
 			connectToSr()
 		},
 	}
 	srAddress = rootCmd.PersistentFlags().StringP("service-registry", "s", "ws://localhost:1234", "Service registry uri")
+	verFlag = rootCmd.PersistentFlags().BoolP("version", "v", false, "Prints version and exit")
 
 	if err := rootCmd.Execute(); err != nil {
 		println(err.Error())
 		os.Exit(-1)
 	}
 
+}
+
+func printVersion() {
+	fmt.Printf("Build time:  		%s\n", buildTime)
+	fmt.Printf("Commit hash: 		%s\n", gitHash)
+	fmt.Printf("Version:     		%s\n", version)
 }
 
 func connectedToSR(w *wango.Wango) {
